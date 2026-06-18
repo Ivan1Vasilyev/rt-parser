603dcbe596f9eb32f6301366236b865cdaf1c324
Ivan Vasilyev <nalivan3747@yandex.ru>
2026-06-18 20:22:59 +0300

    123

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { clusterNamesEnum } from './services/cluster/cluster-models'
 import RootStage from './stages/root/root-stage'
-import { getMainConfig, getInternetConfig, getRanchoConfig, getCitiesOnlyConfig, getConvergentConfig, getMobileConfig, getInternetTvConfig } from './utils/page-config'
+import { getInternetConfig, getRanchoConfig, getCitiesOnlyConfig, getConvergentConfig, getMobileConfig, getInternetTvConfig } from './utils/page-config'
 import readLineService from './services/readline/readline-service'
 import { rootStageNamesEnum } from './stages/root/root-stage-models'
 
@@ -11,11 +11,9 @@ const westCenterMoscow = clusterNamesEnum.westCenterMoscow // Запад, Цен
 
 const clusters = [] as clusterNamesEnum[] // если пустой, парсится всё.
 
-// const mainWayConfig = getMainConfig(clusters)
 const internetWayConfig = getInternetConfig(clusters)
 const ranchoWayConfig = getRanchoConfig(clusters)
 const citiesOnlyConfig = getCitiesOnlyConfig(clusters)
-
 const convergentConfig = getConvergentConfig(clusters)
 const mobileConfig = getMobileConfig(clusters)
 const internetTvConfig = getInternetTvConfig(clusters)
@@ -23,15 +21,12 @@ const internetTvConfig = getInternetTvConfig(clusters)
 const convergentWay = new RootStage(convergentConfig, rootStageNamesEnum.convergent)
 const mobileWay = new RootStage(mobileConfig, rootStageNamesEnum.mobile)
 const internetTvWay = new RootStage(internetTvConfig, rootStageNamesEnum.internetTv)
-
-// const mainWay = new RootStage(mainWayConfig, rootStageNamesEnum.main)
 const internetWay = new RootStage(internetWayConfig, rootStageNamesEnum.internet)
 const ranchoWay = new RootStage(ranchoWayConfig, rootStageNamesEnum.rancho)
 const citiesOnlyWay = new RootStage(citiesOnlyConfig, rootStageNamesEnum.cities)
 
 readLineService.init(internetWay, ranchoWay, citiesOnlyWay, convergentWay, mobileWay, internetTvWay)
 
-// mainWay.go()
 internetWay.go()
 ranchoWay.go()
 // citiesOnlyWay.go()
